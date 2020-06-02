1f03865900e455e13efa12e08a97a6cf58266692
Dwyriel <Dwyriel333@hotmail.com>
2020-06-02 05:53:08 -0300

    Load of bullcrap from imports

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
+
+public class MusicPlayer : MonoBehaviour
+{
+    Scene thisScene;
+    AudioSource aS;
+    bool isCLicked = false;
+    [Range (0,1)][SerializeField] float volume = .15f;
+
+    private void Awake()
+    {
+        Object.DontDestroyOnLoad(aS);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        aS = GetComponent<AudioSource>();
+
+        thisScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aS.volume = volume;
+        if (Input.anyKeyDown && !isCLicked)
+            LoadGame();
+    }
+
+    private void LoadGame()
+    {
+        isCLicked = true;
+        SceneManager.LoadScene(thisScene.buildIndex + 1);
+    }
+}
